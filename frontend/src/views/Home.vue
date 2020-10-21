2e9a95967a8069732ba84d47fc0e28106fe031ba
ianevangelista <ianevangelista1999@gmail.com>
2020-10-21 22:42:47 +0200

    downloaded chart

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,23 +7,46 @@
         <span>{{"Your weight the " + weight.date +' was '}}</span>
         {{weight.weight +' kg'}}
         </h3>
+        <weightChart width="500" type="bar" :options="chartOptions" :series="test"/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Vue from 'vue';
 import Navbar from '.././components/Navbar';
-import {mapState} from 'vuex';
+import {mapGetters} from 'vuex';
+import VueApexCharts from 'vue-apexcharts';
+Vue.use(VueApexCharts);
+
+Vue.component('weightChart', VueApexCharts)
 export default {
   name: 'Home',
   components: {
     Navbar
   },
+  data() {
+    return{
+        chartOptions: {
+          chart: {
+            id: 'vuechart-example'
+          },
+          xaxis: {
+            categories: [],
+          }
+        },
+        series: [{
+          name: 'Your weight',
+          data: []
+        }]
+      }
+    },
   computed: {
-    ...mapState('weight',['allWeights'])
+    ...mapGetters({allWeights: 'weight/getAllWeights'})
   },
+
   created () {
     this.$store.dispatch('weight/loadAllWeights', {root:true});
-  },
+  }
 };
 </script>
